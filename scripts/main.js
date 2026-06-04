398400163f22036d83620d355d4a21551f184a41
Jared092005 <eliseoborja26@gmail.com>
2026-06-04 12:52:21 -0300

    tratando de solucionar el problema con el active

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ let contenedorPaises = document.querySelector("#contenedor-paises");
 for (const pais of paises) {
   let cardGenerado = "";
   cardGenerado += ` <article
-        class="rounded-lg max-w-80 flex flex-col items-center justify-center bg-white dark:bg-[#2c3743] dark:text-white shadow-md active:scale-90 active:transition active:duration-150 md:hover:scale-110 md:transition md:duration-200 md:max-w-65"
+        class="rounded-lg max-w-80 flex flex-col items-center justify-center bg-white dark:bg-[#2c3743] dark:text-white shadow-md active:scale-90 transition duration-150 ease-out md:hover:scale-110 md:transition md:duration-200 md:max-w-65"
       ><div class="w-full h-[60%]">
        <img
           src="${pais.flags.svg}"
@@ -28,7 +28,7 @@ for (const pais of paises) {
         />
         </div>
         <div class="flex flex-col mt-4 gap-2 mb-10 w-full pl-6 font-semibold">
-          <h2 ><span class="font-bold text-2xl cursor-pointer active:text-blue-400 active:transition active:duration-100">${pais.name}</span></h2>
+          <h2 ><span class="font-bold text-2xl cursor-pointer active:text-blue-400">${pais.name}</span></h2>
           <p>Population: <span class="font-normal">${Number(pais.population).toLocaleString("en-US")}</span></p>
           <p>Region: <span class="font-normal">${pais.region}</span></p>
           <p>Capital: <span class="font-normal">${pais.capital}</span></p>
